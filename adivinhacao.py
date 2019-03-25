1aaf3c4bbc4e4e3e494130dadb200505d4e2e8a1
danielsimonebeira <dante_beira@hotmail.com>
2019-03-24 21:45:39 -0300

    Altera o while por for
    alteração do while:
    while(rodada <= total_de_tentativas):
    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
      chute_str = input("Digite o seu numero: ")
      print("Você digitou ", chute_str)
      chute = int(chute_str)
    ...pelo for

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,13 +4,17 @@ print ("*********************************")
 
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
 
-while(rodada <= total_de_tentativas):
+for rodada in range(1,total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite o seu numero: ")
+    chute_str = input("Digite um numero de 1 a 100: ")
     print("Você digitou ", chute_str)
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("----------------------------------------")
+        print("Você deve digitar valores entre 1 e 100.")
+        print("----------------------------------------")
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -22,8 +26,9 @@ while(rodada <= total_de_tentativas):
         print("-------------------------------------------------")
         print("O tipo da variavel acertou é", qualtipo, sep=": ")
         print("-------------------------------------------------")
-        print("VOCÊ ACERTOU !!!")
+        print(" VOCÊ ACERTOU !!!  \o/ \o/ \o/")
         print("-------------------------------------------------")
+        break
     else:
         if(maior):
             print("O seu chute foi maior do que o número secreto!")
